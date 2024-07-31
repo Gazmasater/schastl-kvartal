979958518b4333395ba0f23acd62b9a1c67a2f15
dosmth <imusedto@gmail.com>
2024-08-01 01:00:56 +0300

    footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,5 +2,127 @@ import React, { FC } from 'react'
 import { Styled } from './styled'
 
 export const Footer: FC = () => {
-  return <Styled.Foo>Footer</Styled.Foo>
+  return (
+    <Styled.Footer>
+      <div className="MuiContainer-root MuiContainer-maxWidthXl">
+        <Styled.FooterContainer>
+          <Styled.ImageContainer>
+            <Styled.ImageLink title="Головна сторінка" href="/">
+              <Styled.ImageCustom
+                src="/img/logo-default.svg"
+                width="113"
+                height="128"
+                alt="Логотип компанії 'Білий шоколад'"
+                draggable="false"
+              />
+            </Styled.ImageLink>
+          </Styled.ImageContainer>
+          <Styled.MainFooterGrid className="MuiGrid-root MuiGrid-container">
+            <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-true">
+              <Styled.CopyRightText>© 2024 | ЖК Білий Шоколад - надійний забудовник</Styled.CopyRightText>
+            </div>
+            <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-auto">
+              <Styled.SocialLinksPadding>
+                <Styled.SocialLinksGrid className="MuiGrid-root MuiGrid-container">
+                  <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-auto MuiGrid-grid-md-auto">
+                    <a
+                      className="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit"
+                      aria-disabled="false"
+                      title="Youtube канал"
+                      href="https://www.youtube.com/channel/UCxq0u32tk_mwEFwoS0Toz_w"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="MuiIconButton-label">
+                        <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                          <path
+                            fill="currentColor"
+                            d="M25.282 4.678l-18.563 0c-3.38,0 -6.129,2.759 -6.129,6.148l0 10.348c0,3.39 2.749,6.147 6.129,6.147l18.563 0c3.379,0 6.128,-2.758 6.128,-6.147l0 -10.348c0,-3.389 -2.749,-6.148 -6.128,-6.148zm4.871 16.496c0,2.697 -2.186,4.89 -4.871,4.89l-18.563 0c-2.686,0 -4.871,-2.194 -4.871,-4.89l0 -10.348c0,-2.696 2.185,-4.889 4.871,-4.889l18.563 0c2.685,0 4.871,2.193 4.871,4.889l0 10.348z"
+                          />
+                          <path
+                            fill="currentColor"
+                            d="M21.574 15.773l-8.715 -5.104c-0.194,-0.114 -0.435,-0.116 -0.63,-0.004 -0.196,0.112 -0.317,0.32 -0.317,0.546l0 10.208c0,0.225 0.121,0.434 0.316,0.546 0.097,0.056 0.205,0.083 0.314,0.083 0.109,0 0.219,-0.029 0.317,-0.086l8.715 -5.103c0.192,-0.113 0.311,-0.32 0.311,-0.543 0,-0.223 -0.118,-0.431 -0.311,-0.543zm-8.403 4.549l0 -8.014 6.84 4.007 -6.84 4.007z"
+                          />
+                        </svg>
+                      </span>
+                    </a>
+                  </div>
+                  <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-auto MuiGrid-grid-md-auto">
+                    <a
+                      className="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit"
+                      aria-disabled="false"
+                      title="Facebook паблік"
+                      href="https://www.facebook.com/beliy.shokolad.company"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="MuiIconButton-label">
+                        <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                          <path
+                            fill="currentColor"
+                            d="M20.835 8.29l2.347 0 0 -4.288 -4.267 0c-0.356,-0.014 -4.695,-0.097 -5.82,4.192 -0.017,0.045 -0.376,1.065 -0.376,3.448l-3.903 0.012 0 4.497 3.907 -0.012 0 11.861 5.969 0 0 -11.85 3.95 0 0 -4.509 -3.95 0 0 -1.418c0,-0.882 0.372,-1.933 2.143,-1.933zm0.82 4.338l0 2.534 -3.951 0 0 11.85 -3.993 0 0 -11.863 -3.907 0.011 0 -2.522 3.918 -0.011 -0.011 -0.502c-0.051,-2.524 0.309,-3.586 0.325,-3.637 0.971,-3.694 4.678,-3.508 4.837,-3.498l3.322 0 0 2.313 -1.36 0c-2.308,0 -3.13,1.508 -3.13,2.919l0 2.406 3.95 0z"
+                          />
+                        </svg>
+                      </span>
+                    </a>
+                  </div>
+                  <div className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-auto MuiGrid-grid-md-auto">
+                    <a
+                      className="MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit"
+                      aria-disabled="false"
+                      title="Instagram канал"
+                      href="https://www.instagram.com/biliy_shokolad/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span className="MuiIconButton-label">
+                        <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                          <path
+                            fill="currentColor"
+                            d="M21.485 4.039l-10.97 0c-3.571,0 -6.476,2.905 -6.476,6.476l0 10.971c0,3.57 2.905,6.475 6.476,6.475l10.971 0c3.57,0 6.475,-2.905 6.475,-6.475l0 -10.971c0,-3.571 -2.905,-6.476 -6.476,-6.476l0 0zm5.073 17.447c0,2.797 -2.275,5.072 -5.073,5.072l-10.97 0c-2.798,0 -5.073,-2.275 -5.073,-5.072l0 -10.971c0,-2.798 2.275,-5.073 5.073,-5.073l10.971 0c2.797,0 5.072,2.275 5.072,5.073l0 10.971z"
+                          />
+                          <path
+                            fill="currentColor"
+                            d="M16 9.459c-3.607,0 -6.541,2.934 -6.541,6.541 0,3.607 2.934,6.541 6.541,6.541 3.606,0 6.541,-2.934 6.541,-6.541 0,-3.607 -2.935,-6.541 -6.541,-6.541zm0 11.679c-2.833,0 -5.139,-2.304 -5.139,-5.138 0,-2.833 2.306,-5.138 5.139,-5.138 2.833,0 5.138,2.305 5.138,5.138 0,2.834 -2.305,5.138 -5.138,5.138z"
+                          />
+                        </svg>
+                      </span>
+                    </a>
+                  </div>
+                </Styled.SocialLinksGrid>
+              </Styled.SocialLinksPadding>
+            </div>
+            <Styled.FooterMenuContainer className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-auto">
+              <Styled.FooterMenuGrid className="MuiGrid-root MuiGrid-container">
+                <Styled.FooterMenuItem className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-auto">
+                  <Styled.FooterMenuItemLink
+                    className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+                    href="/about"
+                  >
+                    Про забудовника
+                  </Styled.FooterMenuItemLink>
+                </Styled.FooterMenuItem>
+                <Styled.FooterMenuItem className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-auto">
+                  <Styled.FooterMenuItemLink
+                    className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+                    href="/novyny"
+                  >
+                    Новини та Акції
+                  </Styled.FooterMenuItemLink>
+                </Styled.FooterMenuItem>
+                <Styled.FooterMenuItem className="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-lg-auto">
+                  <Styled.FooterMenuItemLink
+                    className="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary"
+                    href="/kontakty"
+                  >
+                    Контакти
+                  </Styled.FooterMenuItemLink>
+                </Styled.FooterMenuItem>
+              </Styled.FooterMenuGrid>
+            </Styled.FooterMenuContainer>
+          </Styled.MainFooterGrid>
+        </Styled.FooterContainer>
+      </div>
+    </Styled.Footer>
+  )
 }
--- src/components/organisms/Footer/styled.ts
+++ src/components/organisms/Footer/styled.ts
@@ -1,9 +1,187 @@
 import styled from 'styled-components'
 
-const Foo = styled.div`
-  display: none;
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  padding: 3rem 0 2.5rem;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+
+  @media (min-width: 640px) {
+    padding: 3rem 0 3.125rem;
+  }
+
+  @media (min-width: 1280px) {
+    padding: 5.3125rem 0 3.4375rem;
+  }
+`
+
+const FooterContainer = styled.div`
+  position: relative;
+  min-height: 6.25rem;
+  text-align: center;
+
+  @media (min-width: 640px) {
+    padding: 0 0 0 10.3125rem;
+    text-align: left;
+  }
+
+  @media (min-width: 1280px) {
+    padding: 0 0 0 10.1875rem;
+  }
+`
+
+const ImageContainer = styled.div`
+  width: 7.0625rem;
+  margin: 0 auto 2rem;
+
+  @media (min-width: 640px) {
+    top: 0.25rem;
+    left: 0;
+    width: 7.625rem;
+    margin: 0;
+    position: absolute;
+  }
+
+  @media (min-width: 1280px) {
+    top: -1.625rem;
+    width: 7.0625rem;
+  }
+`
+
+const ImageLink = styled.a`
+  display: block;
+  position: relative;
+  padding-top: 113.27434%;
+`
+
+const ImageCustom = styled.img`
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`
+
+const MainFooterGrid = styled.div`
+  width: calc(100% + 1rem);
+  margin: calc(1rem / -2);
+
+  @media (min-width: 640px) {
+    flex-wrap: nowrap;
+  }
+`
+
+const CopyRightText = styled.div`
+  font-size: 0.75rem;
+  line-height: 1.4;
+  margin-left: 0.375rem;
+  letter-spacing: 0.005em;
+  text-transform: uppercase;
+
+  @media (min-width: 1280px) {
+    font-size: 0.875rem;
+    line-height: 1.21429em;
+  }
+`
+
+const SocialLinksPadding = styled.div`
+  margin: -0.875rem 0.1875rem 0;
+
+  @media (min-width: 1280px) {
+    margin: -1.1875rem 3.625rem 0;
+  }
+`
+
+const SocialLinksGrid = styled.div`
+  justify-content: center;
+  width: calc(100% + 0.75rem);
+  margin: calc(0.75rem / -2);
+
+  @media (min-width: 640px) {
+    width: calc(100% + 0.25rem);
+    margin: calc(0.25rrem / -2);
+  }
+
+  @media (min-width: 1280px) {
+    width: calc(100% + 0.75rem);
+    margin: calc(0.75rem / -2);
+  }
+`
+
+const FooterMenuContainer = styled.div`
+  // jss128
+  @media (max-width: 639.95px) {
+    display: none;
+  }
+`
+
+const FooterMenuGrid = styled.div`
+  width: calc(100% + 2rem);
+  margin: calc(2rem / -2);
+`
+
+const FooterMenuItem = styled.div`
+  padding: 0.625rem 0.5rem !important;
+  max-height: 4.125rem;
+
+  @media (min-width: 900px) {
+    padding: 0.625rem 1rem !important;
+  }
+
+  @media (min-width: 1280px) {
+    .jss404 {
+      padding: 0.5rem 1rem !important;
+    }
+  }
+`
+
+const FooterMenuItemLink = styled.a`
+  color: inherit;
+  display: inline-block;
+  padding: 0.5rem 0 0.875rem;
+  transition:
+    border-bottom-color 0.3s,
+    opacity 0.25s;
+  border-bottom: 1px solid transparent;
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  line-height: 1.21429em;
+  text-decoration: none;
+
+  &:active,
+  &:hover,
+  &:focus {
+    text-decoration: none;
+  }
+  &:hover {
+    opacity: 0.9;
+    border-bottom-color: #fccda2;
+  }
+
+  @media (min-width: 360px) {
+    font-size: 0.9375rem;
+    line-height: 1.2em;
+  }
+
+  @media (min-width: 456px) {
+    font-size: 0.875rem;
+    line-height: 1.21429em;
+  }
 `
 
 export const Styled = {
-  Foo,
+  Footer,
+  FooterContainer,
+  ImageContainer,
+  ImageLink,
+  ImageCustom,
+  MainFooterGrid,
+  CopyRightText,
+  SocialLinksPadding,
+  SocialLinksGrid,
+  FooterMenuContainer,
+  FooterMenuGrid,
+  FooterMenuItem,
+  FooterMenuItemLink,
 }
