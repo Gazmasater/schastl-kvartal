7d874018742ea86341c962c5a8bb58b001d14609
dosmth <imusedto@gmail.com>
2024-07-31 22:06:39 +0300

    remove unsused

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import { WidthWrapper, Cards, Spacer } from '@components'
 import { HomeTemplate } from '@templates'
 import { TCards } from '@localTypes/cards'
 
@@ -7,13 +6,9 @@ type THomePageProps = {
   houseTitle: string
 } & TCards
 
-export const HomePage: FC<THomePageProps> = ({ houseTitle, cards }) => (
-  <HomeTemplate>
-    <WidthWrapper>
-      <h1>{houseTitle}</h1>
-      <Spacer size="height-50" />
-      <Cards cards={cards} />
-    </WidthWrapper>
-    <Spacer />
-  </HomeTemplate>
-)
+export const HomePage: FC<THomePageProps> = ({ houseTitle, cards }) => {
+  console.log(houseTitle)
+  console.log(cards)
+
+  return <HomeTemplate />
+}
