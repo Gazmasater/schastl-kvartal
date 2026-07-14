dc326094e54da133e9a340cb36eaa72df7675e09
Lev <tolkoplus@gmail.com>
2026-07-14 20:48:52 +0000

    Revert "Merge pull request #3 from Gazmasater/agent/ssr-styled-components"
    This reverts commit 5e0ff85741570bbfce63184ead51fc0f44f2aad1, reversing
    changes made to 6187613ab69e402995706d614024daba01c5fc0d.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { FC } from 'react'
 import type { AppProps } from 'next/app'
+import StyledComponentsRegistry from '../src/lib/registry'
 import 'normalize.css/normalize.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './styles.css'
 import './miui.css'
 
-const MyApp: FC<any> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const MyApp: FC<any> = ({ Component, pageProps }: AppProps) => (
+  <StyledComponentsRegistry>
+    <Component {...pageProps} />
+  </StyledComponentsRegistry>
+)
 
 export default MyApp
