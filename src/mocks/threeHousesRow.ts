ebab44babe065b523b7078009fb76f4849f15c52
Lev <tolkoplus@gmail.com>
2026-07-15 21:14:17 +0000

    simplify hero content and image alts

--- src/mocks/threeHousesRow.ts
+++ src/mocks/threeHousesRow.ts
@@ -9,7 +9,7 @@ export const ThreeHousesRowMock: TThreeHousesRow = {
     {
       linkUrl: '/doma-v-lipetskom-rayone/dom-skandi-155-m2',
       imgUrl: '/img/threeHousesRow/house-1-155m2/main.webp', // imgUrl: 1078x674
-      imgAlt: '',
+      imgAlt: 'Готовый дом «Сканди» 155 м² в коттеджном посёлке «Счастливый Квартал»',
       title: 'Сканди',
       feautureFirstTitle: 'Площадь общая:',
       featureFirstValue: '155 m2',
@@ -19,7 +19,7 @@ export const ThreeHousesRowMock: TThreeHousesRow = {
     {
       linkUrl: '/doma-v-lipetskom-rayone/barnhaus-115-m2',
       imgUrl: '/img/threeHousesRow/house-2-115m2/main.webp', // imgUrl: 1078x607
-      imgAlt: '',
+      imgAlt: 'Готовый барнхаус 115 м² в коттеджном посёлке «Счастливый Квартал»',
       title: 'Барнхаус',
       feautureFirstTitle: 'Площадь общая:',
       featureFirstValue: '115 m2',
@@ -29,7 +29,7 @@ export const ThreeHousesRowMock: TThreeHousesRow = {
     {
       linkUrl: '/doma-v-lipetskom-rayone/dom-modern-140-m2',
       imgUrl: '/img/threeHousesRow/house-3-140m2/main.webp', // imgUrl: 1078x607
-      imgAlt: '',
+      imgAlt: 'Готовый дом «Модерн-140» 140 м² в коттеджном посёлке «Счастливый Квартал»',
       title: 'Модерн 140',
       feautureFirstTitle: 'Площадь общая:',
       featureFirstValue: '140 m2',
@@ -39,7 +39,7 @@ export const ThreeHousesRowMock: TThreeHousesRow = {
     {
       linkUrl: '/doma-v-lipetskom-rayone/dom-modern-100-m2',
       imgUrl: '/img/threeHousesRow/house-4-97m2/main.webp', // imgUrl: 1078x763
-      imgAlt: '',
+      imgAlt: 'Готовый дом «Модерн-100» 97 м² в коттеджном посёлке «Счастливый Квартал»',
       title: 'Модерн 100',
       feautureFirstTitle: 'Площадь общая:',
       featureFirstValue: '97 m2',
